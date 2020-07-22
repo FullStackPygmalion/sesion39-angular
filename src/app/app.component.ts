8602d3e34c44db4649887cdf0486f41f1d97f2f8
Juan Giovanni Restrepo <grestrepojimenez@gmail.com>
2020-07-21 21:02:38 -0500

    Uso de NgModel, NgIf, NgFor, NgSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sesion39-angular';
+
+  usuarios = ['Alejandro', 'Guillermo', 'Juan']
+
+  addItem(item){
+    this.usuarios.push(item)
+  }
+
+  deleteItem(index: number) {
+      this.usuarios.splice(index, 1)
+  }
+
+
+  products = [
+    {
+      id: '1',
+      image: 'https://image.freepik.com/free-vector/polo-shirt_35735-249.jpg',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'Camiseta tipo polo...'
+    },
+    {
+      id: '2',
+      image: 'https://image.freepik.com/free-vector/polo-shirt_35735-249.jpg',
+      title: 'Camiseta 2',
+      price: 50000,
+      description: 'Camiseta tipo polo...2 '
+    },
+  ]
+
+
+
 }
